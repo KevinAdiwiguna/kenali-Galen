ab407f445a0d267fca9999b55b9a1818cf544d44
KevinAdiwiguna <f1d02410115@student.unram.ac.id>
2025-05-26 18:12:15 +0800

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,17 @@ export default function RootLayout({
 				<meta name="google-adsense-account" content="ca-pub-9394949604148306" />
 				<meta name="google-site-verification" content="ca-pub-9394949604148306" />
 
+				<Script async src="https://www.googletagmanager.com/gtag/js?id=G-S21QKK1FW3"></Script>
+				<Script>
+					{`
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+
+					gtag('config', 'G-S21QKK1FW3');
+  					`}
+				</Script>
+
 				<Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9394949604148306" crossOrigin="anonymous" strategy="afterInteractive" />
 			</head>
 			<body className={inter.className}>
